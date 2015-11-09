d85ad0dff2d1151e30f50077f84f128e673890cf
corbe <root@localhost.corbe.us>
2015-11-08 22:21:53 -0400

    Fix Geojson

--- abrigo/views.py
+++ abrigo/views.py
@@ -62,12 +62,15 @@ def getlocation(request,place_id):
 
 def get(request):    
     arr=[]
-    for obj in AbrigoLocation.objects.all():       
+
+    for abrigo in Abrigo.objects.all(): 
+       obj= AbrigoLocation.objects.get(abrigo=abrigo)
        addresses_components =[] 
        for address_component in AddressComponent.objects.filter(location__pk=obj.location.pk):
            types=[]
            for t in address_component.types.all():
                types.append(t.name.encode("utf-8"))
+           
            addresses_components.append(
                 {
                     "long_name": address_component.long_name.encode("utf-8"),
@@ -76,42 +79,42 @@ def get(request):
                     }
                 )
 
-           objJson = {
-               "address_components": addresses_components,
-               "formatted_address": obj.location.formatted_address,
-               "geometry": {
-                   "location": {
-                       "lat": obj.location.lat,
-                       "lng": obj.location.lng
+       objJson = {
+           "address_components": addresses_components,
+           "formatted_address": obj.location.formatted_address,
+           "geometry": {
+               "location": {
+                   "lat": obj.location.lat,
+                   "lng": obj.location.lng
+                   },
+               "location_type": "ROOFTOP",
+               "viewport": {
+                   "northeast": {
+                       "lat": obj.location.viewport_northeast_lat,
+                       "lng": obj.location.viewport_northeast_lng
                        },
-                   "location_type": "ROOFTOP",
-                   "viewport": {
-                       "northeast": {
-                           "lat": obj.location.viewport_northeast_lat,
-                           "lng": obj.location.viewport_northeast_lng
-                           },
-                       "southwest": {
-                           "lat": obj.location.viewport_southwest_lat,
-                           "lng": obj.location.viewport_southwest_lng
-                           }
-                       },
-                   "bounds": {
-                       "northeast": {
-                           "lat": obj.location.bound_northeast_lat,
-                           "lng": obj.location.bound_northeast_lng
-                           },
-                       "southwest": {
-                           "lat": obj.location.bound_southwest_lat,
-                           "lng": obj.location.bound_southwest_lng
-                           }
+                   "southwest": {
+                       "lat": obj.location.viewport_southwest_lat,
+                       "lng": obj.location.viewport_southwest_lng
                        }
                    },
-               "place_id": str(obj.location.place_id),
-               "types": [
-                   "street_address"
-                   ]
-               }
-           arr.append(objJson)              
+               "bounds": {
+                   "northeast": {
+                       "lat": obj.location.bound_northeast_lat,
+                       "lng": obj.location.bound_northeast_lng
+                       },
+                   "southwest": {
+                       "lat": obj.location.bound_southwest_lat,
+                       "lng": obj.location.bound_southwest_lng
+                       }
+                   }
+               },
+           "place_id": str(obj.location.place_id),
+           "types": [
+               "street_address"
+               ]
+           }
+       arr.append(objJson)              
     
     return HttpResponse(json.dumps(arr).replace("'","\""))
   
